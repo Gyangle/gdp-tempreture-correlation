c99eed46614f2b218aeb91ae6c11d9fded599739
Gyangle <gyangle@umich.edu>
2021-12-01 13:44:43 -0500

    add python starter code

---
+++ main.py
@@ -0,0 +1,36 @@
+import sqlite3
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+
+# this function compares the number between Dumbledore's Army and Death eaters
+# chart: group names vs. population
+def wizard_war_number_compare():
+    pass
+
+
+# this function compares the box office for each year
+# chart: name of movie(year) vs. box office
+def movie_box_office():
+    pass
+
+
+# survival rate of each house
+# chart: house names vs. survival rate
+def house_survival_rate():
+    pass
+
+
+def main():
+    wizard_war_number_compare()
+    movie_box_office()
+    house_survival_rate()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
