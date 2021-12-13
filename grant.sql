391e3a71c146ad76b57db2fb7f1fe87a100c45bb
GrantY <gyangle@umich.edu>
2021-12-13 13:43:10 -0500

    run to include txt files

--- grant.sql
+++
@@ -1,6 +0,0 @@
-SELECT CountryName.c_name, Temperature.temp
-FROM CountryCode  JOIN CountryName ON CountryCode.id = CountryName.id, Temperature, CountryGDP
-WHERE Temperature.country_name LIKE  ('%'||CountryName.c_name||'%')
-AND CountryGDP.c_code = CountryCode.c_code
-ORDER BY CountryGDP.c_GDP DESC
-LIMIT 10
